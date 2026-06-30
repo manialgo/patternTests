a750043af7c832f53c744980c93b9dea44c35e92
Manikandan M <manimmaths@gmail.com>
2026-06-30 16:53:35 +0530

    Problem Statement included
    Problem's brief description and the example of test constraints are included.

--- StringGame.java
+++ StringGame.java
@@ -39,3 +39,47 @@ public class StringGame{
 		sc.close();
 	}
 }
+
+
+/*
+Given a string s and a dictionary of string wordDict of size n, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
+
+Note: The same word in the dictionary may be reused multiple times in the segmentation. & All the strings of wordDict are unique.
+
+Input format :
+The first line of input is a string 's' consisting of lowercase English letters.
+The second line of input is an integer 'n' represents the size of the dictionary.
+The next n lines consists of strings denoting the words in the dictionary.
+
+Output format :
+The output displays a list of strings, where each string is a space-separated sequence of dictionary words that form the input string s.
+If no such sequence exists, an empty list is returned.
+
+Code constraints :
+1 <= n <= 10
+The string s contains both uppercase and lowercase.
+
+Example:-
+
+Input:
+s = "catsanddog"
+n = 5
+wordDict = ["cat","cats","and","sand","dog"]
+
+Output:
+["cats and dog","cat sand dog"]
+
+Sample test cases :-
+
+Input:
+catsanddog
+5
+cat
+cats
+and
+sand
+dog
+
+Output:
+[cats and dog, cat sand dog]
+*/
