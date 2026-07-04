0b4fcc6dd0f83370c19765ab25d477917fe88283
Manikandan M <manimmaths@gmail.com>
2026-07-04 14:36:41 +0530

    Add comments for capitalizeWords functionality
    Added problem statement and input/output format comments for the capitalizeWords function.

--- src/test19062026/CapitalizeWord.java
+++ src/test19062026/CapitalizeWord.java
@@ -23,3 +23,22 @@ public class CapitalizeWord {
 		sc.close();
 	}
 }
+
+/*
+
+Problem Statement:-
+Vijay is working in a software company and he is preparing a document about his project. As per requirement, the first letter in each word should be in the upper case. But Vijay typed the entire document in lowercase. Help Vijay to capitalize the first letter in each word of a sentence.
+
+Input format :-
+The input consists of a sentence.
+
+Output format :-
+The output prints the string with the first letter in upper case.
+
+Sample test cases :-
+Input 1 :-
+a quick brown fox jumps over the lazy dog
+Output 1 :-
+A Quick Brown Fox Jumps Over The Lazy Dog 
+
+*/
