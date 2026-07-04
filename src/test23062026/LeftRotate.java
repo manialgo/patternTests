ee73d91461a1fb893cc61102613113b61cd9027c
Manikandan M <manimmaths@gmail.com>
2026-07-04 14:56:07 +0530

    Update LeftRotate.java

--- src/test23062026/LeftRotate.java
+++ src/test23062026/LeftRotate.java
@@ -1,7 +1,7 @@
 package test23062026;
 
 import java.util.Scanner;
-class LeftRotate{
+public class LeftRotate{
     private static int[] leftRotate(int[] arr){
         int temp = arr[0];
         for(int i=0; i<arr.length-1; i++)   arr[i] = arr[i+1];
@@ -18,3 +18,41 @@ class LeftRotate{
         sc.close();
     }
 }
+/*
+
+Problem Statement:-
+Write a program that will cycle all elements of an array from one position to the left.  
+
+Example:-
+Input:-
+8
+-1 2 -4 3 7 6 102 41
+
+Output:-
+2 -4 3 7 6 102 41 -1
+
+Input format :-
+The first line of the input consists of the value of n.
+The next input is the array elements.
+
+Output format :-
+The output prints the resultant array.
+
+Note: There is an extra space at the end of the output.
+
+Refer to the sample output for formatting specifications.
+
+Sample test cases :-
+Input 1 :-
+8
+-1 2 -4 3 7 6 102 41
+Output 1 :-
+2 -4 3 7 6 102 41 -1 
+
+Input 2 :-
+10
+15 20 36 98 45 12 77 48 96 100
+Output 2 :-
+20 36 98 45 12 77 48 96 100 15
+
+*/
