4d1de6b2acd2e7136bf9b73b0d9f109cc1bfe315
Manikandan M <manimmaths@gmail.com>
2026-07-04 14:53:43 +0530

    Add problem statement and examples to FirstRepeativeElement.java
    Added a problem statement and sample test cases for finding the first repeating element in an array.

--- src/test22062026/FirstRepeativeElement.java
+++ src/test22062026/FirstRepeativeElement.java
@@ -20,3 +20,45 @@ public class FirstRepeativeElement {
 		sc.close();
 	}
 }
+
+/*
+
+Problem Statement:-
+Given an array of numbers. Give an algorithm for finding the first element in the array which is repeated.
+
+Input format :-
+The first line of the input consists of an integer representing the size of array 'n'.
+The second line of the input consists of n elements representing the array elements 'e' separated by space.
+
+Output format :-
+The output should display the first repeating element in the array as
+
+"The first repeating element is <<element>>" or "There are no repeating elements"
+
+Refer to the sample output for the formatting specifications.
+
+Code constraints :-
+In this scenario, the test cases fall under the following constraints:
+1 ≤ n ≤ 10
+1 ≤ e ≤ 100
+
+Sample test cases :-
+Input 1 :-
+7
+10 5 3 5 3 4 6
+Output 1 :-
+The first repeating element is 5
+
+Input 2 :-
+5
+1 5 6 8 7
+Output 2 :-
+There are no repeating elements
+
+Input 3 :-
+8
+5 2 4 6 7 2 4 5
+Output 3 :-
+The first repeating element is 5
+
+*/
