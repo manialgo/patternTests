a3a2f911a8c4e3c5ed89aaa8cfbda99938690fc0
Manikandan M <manimmaths@gmail.com>
2026-07-04 11:45:38 +0530

    Add problem statement and examples to ReverseANumber.java
    Added problem statement and sample test cases for reversing a number.

--- src/test13062026/ReverseANumber.java
+++ src/test13062026/ReverseANumber.java
@@ -18,3 +18,29 @@ public class ReverseANumber {
 	        sc.close();
 	    }
 }
+
+/*
+
+Problem Statement:-
+In the Mini Project, the 7th module is to reverse a number. Rita allotted this task to Malini. Test the program and report the results obtained. Help Malini to write a program to reverse a number. 
+
+Input format :-
+The input consists of an integer.
+
+Output format :-
+The output prints the reverse of the number.
+
+Refer to the sample output for formatting specifications.
+
+Sample test cases :-
+Input 1 :-
+123
+Output 1 :-
+321
+
+Input 2 :-
+4567
+Output 2 :-
+7654
+
+*/
