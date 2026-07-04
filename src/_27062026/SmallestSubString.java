fb49a7f5275e1ec66cf8d29dc449aa452c1e800f
Manikandan M <manimmaths@gmail.com>
2026-07-04 11:07:00 +0530

    Update Problem Statement
    Added problem statement and example test cases for finding the smallest subarray with the same degree as the input array.

--- src/_27062026/SmallestSubString.java
+++ src/_27062026/SmallestSubString.java
@@ -40,3 +40,52 @@ public class SmallestSubString {
 		sc.close();
 	}
 }
+
+/*
+
+Problem Statement:-
+
+Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements. Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
+
+Example:-
+
+Input
+n = 5, nums = [1,2,2,3,1]
+Output
+2
+
+Explanation:-
+
+The input array has a degree of 2 because both elements 1 and 2 appear twice.
+Of the subarrays that have the same degree:
+[1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+The shortest length is 2. So return 2.
+
+
+Input format :-
+The first line of the input is the length of the array.
+The second line of the input is the space-separated array of integer elements.
+
+Output format :-
+The output is the integer element representing the degree of the element.
+
+Refer to the sample input and output for formatting specifications.
+
+Code constraints :-
+nums length will be between 1 and 50,000.
+nums[i] will be an integer between 0 and 49,999.
+
+Sample test cases :-
+Input 1 :-
+5
+1 2 2 3 1
+Output 1 :-
+2
+
+Input 2 :-
+7
+1 2 2 3 1 4 2
+Output 2 :-
+6
+
+*/
