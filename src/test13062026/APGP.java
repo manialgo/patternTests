137d5fc95e84fbc6ffd96f6439b4ec18bea3176d
Manikandan M <manimmaths@gmail.com>
2026-07-04 11:43:01 +0530

    Add problem statement for sequence type identification
    Added problem statement and example cases for sequence identification.

--- src/test13062026/APGP.java
+++ src/test13062026/APGP.java
@@ -25,3 +25,50 @@ public class APGP {
     }
 
 }
+/*
+
+Problem Statement:-
+Tamil isn't really good at math, but his sadistic teacher still gave him some math exercises about sequences. He needs to find the next number of the given sequence, which can be one of the following two types:
+1.Arithmetic progression
+2.Geometric progression
+
+You are given numbers in the series as a string n, in which the numbers are separated by commas. Write a program to help Tharun to figure out what type of sequence it is, and return the next number.
+
+Note:-
+The input string contains a minimum of four integers separated by a comma.
+   1) Arithmetic Progression - It is a sequence of numbers such that the difference between the consecutive terms is constant.
+   2) Geometric progression - It is a sequence of numbers where each term after the first is found by multiplying the previous one by a fixed, non-zero number called the common ratio.
+
+Example 1:-
+
+Input
+4
+2 4 6 8
+
+Output:-
+10
+
+Explanation:-
+For sequence = "5,10,20,40,80", the output should be 160.
+The sequence has a common ratio, r = 2.
+
+Input format :-
+The first line consists of a string that denotes n.
+
+Output format :-
+The output is an integer that denotes the number which completes the sequence
+
+Sample test cases :-
+Input 1 :-
+4
+2 4 6 8
+Output 1 :-
+10
+
+Input 2 :-
+5
+5 10 20 40 80
+Output 2 :-
+160
+
+*/
