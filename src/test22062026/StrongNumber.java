58e444cc93fb5a55cb700aeafefe98a96bbe94eb
Manikandan M <manimmaths@gmail.com>
2026-07-04 14:47:11 +0530

    Update StrongNumber.java

--- src/test22062026/StrongNumber.java
+++ src/test22062026/StrongNumber.java
@@ -23,3 +23,42 @@ public class StrongNumber{
         sc.close();
     }
 }
+
+/*
+
+Problem Statement:-
+The factorial concept was explained in Seetha's math class. Ankit discovered a connection between the sum of the factorial of digits in a number and the number itself when experimenting with numbers after understanding the idea of a factorial.
+
+He began looking for additional numbers like this since he was so excited. Write a suitable program to check the supplied numbers to aid Ankit.
+
+Example:-
+Input:-
+145
+Output:-
+145 Strong Number
+
+In this case, the program reads the input integer 145 and calculates the factorial of each digit of the number (1! + 4! + 5!). It then checks if the sum of the factorials is equal to the original number, which is true for 145, so the program prints "145 Strong Number" on the console as the output.
+
+Input format :-
+The input consists of n as an integer.
+
+Output format :-
+The output displays the input integer followed by Strong Number or Not Strong Number.
+
+Refer to the sample output for the formatting specifications.
+
+Code constraints :-
+1 <= n <= 105
+
+Sample test cases :-
+Input 1 :-
+145
+Output 1 :-
+145 Strong Number
+
+Input 2 :-
+140
+Output 2 :-
+140 Not Strong Number
+
+*/
