4eb56df4f700c5e2233a2726179cefd665491a04
Manikandan M <manimmaths@gmail.com>
2026-07-04 12:51:55 +0530

    Document pattern search problem statement in PatternText.java
    Added problem statement and example inputs/outputs for pattern searching.

--- src/test17062026/PatternText.java
+++ src/test17062026/PatternText.java
@@ -33,3 +33,64 @@ public class PatternText {
         
     }
 }
+
+/*
+
+Problem Statement:-
+Given a text txt [0 . . . N-1] and a pattern pat [0 . . . M-1], write a function search (char pat [], char txt []) that prints all occurrences of pat [] in txt []. You may assume that N > M. Use zero-based indexing while returning the indices. 
+
+Example:-
+Input:-
+txt [] = “THIS IS A TEST TEXT”, pat [] = “TEST”
+
+Output:-
+Pattern found at index 10
+
+Input:-
+txt [] = “AABAACAADAABAABA”, pat [] = “AABA”
+
+Output:-
+Pattern found at index 0, Pattern found at index 9, Pattern found at index 12
+
+Input format :-
+The first line of input consists of a string txt, in which the pattern is to be searched.
+The second line of input consists of a string pat, the pattern that needs to be found in the text.
+
+Output format :-
+For each occurrence of the pattern in the text, print "Pattern found at index X" in a new line, where X the indices where the pattern is found within the text string.
+
+Refer to the sample output for the formatting specifications.
+
+Code constraints :-
+The text and the pattern will only contain uppercase and lowercase English letters.
+1 ≤ length of txt ≤ 50
+1 ≤ length of pat ≤ 10
+
+Sample test cases :-
+Input 1 :-
+THIS IS A TEST TEXT
+TEST
+Output 1 :-
+Pattern found at index 10
+
+Input 2 :-
+AABAACAADAABAABA
+AABA
+Output 2 :-
+Pattern found at index 0
+Pattern found at index 9
+Pattern found at index 12
+
+Input 3 :-
+agd
+g
+Output 3 :-
+Pattern found at index 1
+
+Input 4 :-
+ABABDABACDABABCABAB
+ABABCABAB
+Output 4 :-
+Pattern found at index 10
+
+*/
