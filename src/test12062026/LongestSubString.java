1f4b5838d2f3f6f2435f1dd49acc1d00747f867a
Manikandan M <manimmaths@gmail.com>
2026-07-04 11:22:06 +0530

    Add problem statement and examples to LongestSubString.java
    Added problem statement and examples for longest substring calculation.

--- src/test12062026/LongestSubString.java
+++ src/test12062026/LongestSubString.java
@@ -27,3 +27,60 @@ public class LongestSubString {
 		sc.close();
 	}
 }
+
+/*
+
+Problem Statement:-
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:-
+
+Input: s = "abcabcbb"
+
+Output: 3
+
+Explanation:-
+The answer is "abc", with a length of 3.
+
+Example 2:-
+
+Input: s = "bbbbb"
+
+Output: 1
+
+Explanation:-
+The answer is "b", with a length of 1.     
+
+Example 3:-
+
+Input: s = "pwwkew"
+
+Output: 3
+
+Explanation:-
+The answer is "wke", with a length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.                                                                                                        
+
+Input format :-
+The input consists of a string s.
+
+Output format :-
+The output prints the length of the longest substring without repeating characters.
+
+Refer to the sample output for the formatting specifications.
+
+Code constraints :-
+1 ≤ s.length ≤ 100
+
+Sample test cases :-
+Input 1 :-
+abcabcbb
+utput 1 :-
+3
+
+Input 2 :-
+bbbb
+Output 2 :-
+1 
+
+*/
