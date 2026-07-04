8e100086013ae4b149954f357c5f9059464152ce
Manikandan M <manimmaths@gmail.com>
2026-07-04 11:52:43 +0530

    Implement swimming pool area calculation program
    Added a Java program to calculate areas of circular and rectangular swimming pools without using conditional statements. It also determines which pool has the larger area based on user input.

--- src/test13062026/SwimmingPool.java
+++ src/test13062026/SwimmingPool.java
@@ -26,3 +26,47 @@ public class SwimmingPool {
 
 
 }
+
+/*
+
+Problem Statement:-
+Arshad works on a project to construct a 20-story building. In that building, the swimming pool area should be circular or rectangular in shape. If it is circular, then the radius of the swimming pool is X meters. If it is rectangular, then the length and breadth of the swimming pool are Y and Z meters. Arshad has to know the total area of the swimming pool (both circular and rectangular). Write a Java program to find the total area of the swimming pool (both circular and rectangular shapes) and print which swimming pool covers the largest area. The pi value is 3.14.
+
+[Note: don’t use if else statement]
+
+Input format :-
+The first line of the input consists of a value which is the radius of the circular swimming pool
+The second and third line consists of the length and breadth of the rectangular swimming pool
+
+Output format :-
+The first and second lines of the output consist of a double value with only two numbers after the decimal point.
+The third line is a string that represents the largest area of the swimming pool, like a circular or rectangular one.
+
+Refer to the sample output for format specifications.
+
+Sample test cases :-
+Input 1 :-
+75
+100
+75
+Output 1 :-
+471.00
+350.00
+Circular Swimming pool covers larger area
+
+Input 2 :-
+2
+5
+6
+Output 2 :-
+12.56
+22.00
+Rectangular swimming pool has larger area
+
+Blacklist:-
+if
+else
+elif
+else if
+
+*/
