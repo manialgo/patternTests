3a4d1edbbee2937f1dc1e2272219ad5c08dae387
Manikandan M <manimmaths@gmail.com>
2026-07-04 14:42:44 +0530

    Refactor ChristmasCelebration class and add comments
    Updated class declaration and added problem statement as comments.

--- src/test20062026/ChristmasCelebration.java
+++ src/test20062026/ChristmasCelebration.java
@@ -1,7 +1,7 @@
 package test20062026;
 
 import java.util.Scanner;
-class ChristmasCelebration{
+public class ChristmasCelebration{
     public static void buyPacket(int N, int M){
         if(M % N == 0)  System.out.println("Yes");
         else System.out.println("No");
@@ -14,3 +14,26 @@ class ChristmasCelebration{
         sc.close();
     }
 }
+
+/*
+
+Problem Statement:-
+Christmas is in next month. This time John is planning to celebrate it differently. So, he decides to distribute some gifts to all children in the street.He went to a shop to buy a packet of chocolates.
+
+At the chocolate shop, each packet is having a different number of chocolates. He wants to buy such a packet which contains the number of chocolates, which can be distributed equally among all of his street children.
+
+Being a friend of John, help him to buy such a packet.
+
+Input format :-
+Each test case contains two integers, N and M, where is N is the number of children in his street and M is the number of chocolates in a packet.
+
+Output format :-
+In each case output "Yes" if he can buy that packet and "No" if he can't buy that packet.(Output is case sensitive)
+
+Sample test cases :-
+Input 1 :-
+4 17
+Output 1 :-
+No
+
+*/
