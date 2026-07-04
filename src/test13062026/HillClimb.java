cd0db0d14150b995144e597d346faac71e05102a
Manikandan M <manimmaths@gmail.com>
2026-07-04 11:44:34 +0530

    Add problem statement for HillClimb.java
    Added problem statement and example for climbing steps.

--- src/test13062026/HillClimb.java
+++ src/test13062026/HillClimb.java
@@ -24,3 +24,44 @@ public class HillClimb {
 		sc.close();
 	}
 }
+
+/*
+
+Problem Statement:-
+Ram is climbing a Hill temple that contains steps that are made of stones. There are total 'n' steps on the hill.
+Each time he can either climb 1 or 2 steps. In how many distinct ways can he climb to the top?
+
+Write a program to find the number of distinct ways to reach the top.
+
+Note:-
+Given n will be a positive integer.
+
+Example:-
+
+Input:-
+3
+
+Output:-
+3
+
+Explanation:-
+The number of steps = 3.
+The distinct ways to climb to the top are:
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+Therefore there are 3 distinct ways.
+
+Input format :-
+The first line contains a single integer the denotes the number of steps, n.
+
+Output format :-
+The output is an integer that denotes the number of ways to climb to the top.
+
+Sample test cases :-
+Input 1 :-
+3
+Output 1 :-
+3
+
+*/
