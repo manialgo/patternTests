da6698bb56297b3721b8a1af50a571adeda83548
Manikandan M <manimmaths@gmail.com>
2026-07-10 11:05:51 +0530

    Update and rename TotalNumberOfProples.java to TotalNumberOfPeoples.java

--- src/test07072026/TotalNumberOfPeoples.java
+++ src/test07072026/TotalNumberOfPeoples.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class TotalNumberOfProples{
+public class TotalNumberOfPeoples{
     public static void main(String[] args){
         Scanner sc =  new Scanner(System.in);
         int girls = sc.nextInt();
